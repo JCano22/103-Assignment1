1086e077546cc6bedf7c60c25bdefceb9893612a
Jorge Cano <coronero@gmail.com>
2023-02-28 18:32:26 -0800

    Assignment1

---
+++ script.js
@@ -0,0 +1,17 @@
+//YouTube
+let siteName = "YouTube";
+let  userName = "enter email";
+let menu = "Menu";
+let recomended = "Recomended Videos";
+let cont = "Continue watching"; 
+let subs = "Subscriptions";
+let history = "History";
+let logoutBtn = "Logout";
+
+
+let title = "Video Title"
+let length = "Video Length";
+let comments = "Video Comments";
+let like = true; //boolean variable for like/dislike
+let shareBtn = "Share";
+let notifyBtn = "Notificiations Button";
